327caf93cead25d562d1de544493ae0e8116f31b
七号铁轨 <1913531239@qq.com>
2020-07-04 15:19:15 +0800

    创建分支

--- HelloGit/src/main/java/git/HelloGit.java
+++ HelloGit/src/main/java/git/HelloGit.java
@@ -5,8 +5,9 @@ public class HelloGit {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("创建分支");
-         System.out.println("在线更新");
+        System.out.println("在线更新");
         System.out.println("克隆");
+        System.out.println("test ssh");
     }
 
 }
