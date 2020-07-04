761240108441d03f050169cc997c35a84c6f4d7f
七号铁轨 <1913531239@qq.com>
2020-07-04 11:50:48 +0800

    克隆

--- HelloGit/src/main/java/git/HelloGit.java
+++ HelloGit/src/main/java/git/HelloGit.java
@@ -6,6 +6,7 @@ public class HelloGit {
         System.out.println("hello git");
         System.out.println("创建分支");
          System.out.println("在线更新");
+        System.out.println("克隆");
     }
 
 }
